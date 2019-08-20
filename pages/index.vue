396b20af59f52948042d2a7e6bbf54f5c144d25c
Jennifer Omena <jennifer.omena@m4u.com.br>
2019-08-19 22:06:50 -0300

    primeiro commit

---
+++ pages/index.vue
@@ -0,0 +1,78 @@
+<template>
+  <div class="container">
+    <div>
+      <logo />
+      <h1 class="title">Unicarioca TI</h1>
+      <h2 class="subtitle">Em construção ...</h2>
+      <div class="links">
+        <a
+          href="https://lnkd.in/eJAqGKb/"
+          target="_blank"
+          class="button--green"
+          >GitHub
+        </a>
+        <a
+          href="https://www.linkedin.com/company/unicariocati/"
+          target="_blank"
+          class="button--green"
+          >LinkedIn
+        </a>
+        <a
+          href="https://discord.gg/HfPvDnN/"
+          target="_blank"
+          class="button--green"
+          >Discord
+        </a>
+        <a
+          href="https://t.me/unicariocati/"
+          target="_blank"
+          class="button--green"
+          >Telegram
+        </a>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import Logo from '~/components/Logo.vue'
+
+export default {
+  components: {
+    Logo
+  }
+}
+</script>
+
+<style>
+.container {
+  margin: 0 auto;
+  min-height: 100vh;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  text-align: center;
+}
+
+.title {
+  font-family: 'Quicksand', 'Source Sans Pro', -apple-system, BlinkMacSystemFont,
+    'Segoe UI', Roboto, 'Helvetica Neue', Arial, sans-serif;
+  display: block;
+  font-weight: 300;
+  font-size: 100px;
+  color: #35495e;
+  letter-spacing: 1px;
+}
+
+.subtitle {
+  font-weight: 300;
+  font-size: 42px;
+  color: #526488;
+  word-spacing: 5px;
+  padding-bottom: 15px;
+}
+
+.links {
+  padding-top: 15px;
+}
+</style>
